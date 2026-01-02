f50ded35e83955d542cf382ac5422816e984f558
andysaedah <microstars@gnail.com>
2026-01-03 00:03:32 +0800

    Fix amendment report button not resetting on success
    Added button reset (disabled=false, textContent=original) before
    closing modal on successful submission

--- church.php
+++ church.php
@@ -400,16 +400,16 @@ require_once 'includes/header.php';
                     <?php endif; ?>
 
                     <?php /* Twitter hidden for now
-          <?php if (!empty($church['twitter'])): ?>
-              <a href="https://twitter.com/<?php echo htmlspecialchars($church['twitter']); ?>" target="_blank"
-                  rel="noopener" class="social-link-large" title="Twitter">
-                  <svg viewBox="0 0 24 24" fill="currentColor">
-                      <path
-                          d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
-                  </svg>
-              </a>
-          <?php endif; ?>
-          */ ?>
+         <?php if (!empty($church['twitter'])): ?>
+             <a href="https://twitter.com/<?php echo htmlspecialchars($church['twitter']); ?>" target="_blank"
+                 rel="noopener" class="social-link-large" title="Twitter">
+                 <svg viewBox="0 0 24 24" fill="currentColor">
+                     <path
+                         d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
+                 </svg>
+             </a>
+         <?php endif; ?>
+         */ ?>
                 </div>
             </div>
         <?php endif; ?>
@@ -1274,6 +1274,8 @@ require_once 'includes/header.php';
             const data = await response.json();
 
             if (data.success) {
+                btn.disabled = false;
+                btn.textContent = originalText;
                 closeReportModal();
                 showToast(data.message || '<?php _e('amendment_reported_success'); ?>', 'success');
             } else {
